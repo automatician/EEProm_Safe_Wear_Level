4ab958756a0dd68c269f17582ecc002ec0283284
automatician <roburkatze@gmail.com>
2025-10-16 13:00:58 +0200

    Update demo5_log_functions.ino

--- examples/demo5_log_functions.ino
+++ examples/demo5_log_functions.ino
@@ -1,7 +1,7 @@
 // #############################################
 // ########### Demo5: log functions ############
 // #############################################
-// EEProm_Safe_Wear_Level Library v25.10.5
+// EEProm_Safe_Wear_Level Library v25.10.x
 // #############################################
 // 
 // This demo demonstrates the use of the logging functionality. 
